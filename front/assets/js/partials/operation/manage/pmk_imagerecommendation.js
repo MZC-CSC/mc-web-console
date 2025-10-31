f86adb9a63d88bd8738089198df6d8db7d8a94aa
SungWoongz <ftsw0221@naver.com>
2025-10-31 16:42:47 +0900

    fix: Update isKubernetesImage parameter in API service and recommendation script
    - Restore isKubernetesImage parameter in mci_api.js to ensure proper functionality.
    - Change default value of isKubernetesImage to true in pmk_imagerecommendation.js for accurate image recommendations.

--- front/assets/js/partials/operation/manage/pmk_imagerecommendation.js
+++ front/assets/js/partials/operation/manage/pmk_imagerecommendation.js
@@ -208,7 +208,7 @@ export async function getRecommendImageInfoPmk() {
 		var searchParams = {
 			includeDeprecatedImage: false,
 			isGPUImage: isGPUImage === "true",
-			isKubernetesImage: false,
+			isKubernetesImage: true,
 			isRegisteredByAsset: false,
 			osArchitecture: osArchitecture,
 			osType: osType,
